213e816e6003292603d495fa634b8d9220340242
aylin <aylin@aylin-VirtualBox>
2019-04-24 19:55:42 -0600

    AdvancedOperations

--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/controller/ProductoController.java
@@ -5,7 +5,9 @@ import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service.ProductoServic
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,15 +21,19 @@ public class ProductoController {
     
     @GetMapping("/buscarTodos")
     public List<ProductoEntity> buscarTodos() {   
-        //ArryList<PsroductoEntity> lista = new ArryList<ProductoEntity>();
         return this.productoService.buscarTodos();
     }
     
+    
     @PostMapping("/registrarProducto")
-    public ProductoEntity create(@RequestBody(required = true) ProductoEntity newProducto){
+    public String create(@RequestBody(required = true) ProductoEntity newProducto){
         
         return this.productoService.registrar(newProducto);
     }
-    
-    
+    ///actualizarStock/{id}/{opcion}/{stock}
+    @PutMapping("/actualizarStock/{id}/{operacion}/{stock}")
+    public ProductoEntity updateStock(@PathVariable(required = true) Long id, @PathVariable(required = true) String operacion, @PathVariable(required = true) Integer stock) {
+        return this.productoService.actualizarStock(id, operacion, stock);
+}
+  
 }
--- parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
+++ parcial2_inventario/src/main/java/gt/edu/umg/ingenieria/sistemas/parcial2/inventario/service/ProductoService.java
@@ -2,6 +2,9 @@ package gt.edu.umg.ingenieria.sistemas.parcial2.inventario.service;
 
 import gt.edu.umg.ingenieria.sistemas.core.parcial2.core.model.ProductoEntity;
 import gt.edu.umg.ingenieria.sistemas.parcial2.inventario.dao.ProductoRepository;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,16 +16,74 @@ public class ProductoService {
     private ProductoRepository productoRepository;
     
     public List<ProductoEntity> buscarTodos() {
-        return (List<ProductoEntity>) this.productoRepository.findAll();
         
-       // ArryList<ProductoEntity> lista = new ArryList<ProductoEntity>();
+        ArrayList<ProductoEntity> productos = new ArrayList<ProductoEntity>();
         
+        productos = (ArrayList<ProductoEntity>) this.productoRepository.findAll();      
+        Collections.sort(productos, new Comparator<ProductoEntity>() {
+            public int compare(ProductoEntity obj1, ProductoEntity obj2) {
+                return obj1.getName().compareTo(obj2.getName());
+            }
+        });
+        
+        
+     return productos;   
     }
     
-
-    public ProductoEntity  registrar(ProductoEntity producto){
-        return this.productoRepository.save(producto);
+     public ProductoEntity actualizarStock(Long id, String opcion,Integer stock) {
+       
+        ProductoEntity producto = this.productoRepository.findById(id).get();
         
+        
+        Integer stockActual=0;
+        Integer Nuevostock=0;
+        
+        if (opcion.equals("incrementar")) 
+        {
+            stockActual = producto.getStock();
+            Nuevostock = stockActual + stock;
+            producto.setStock(Nuevostock);
+        } 
+        else if (opcion.equals("decrementar"))
+        {
+            stockActual = producto.getStock();
+            Nuevostock = stockActual - stock;
+            producto.setStock(Nuevostock);
+        }
+         
+        return producto;
+     }
+        
+        
+
+
+   // public ProductoEntity  registrar(ProductoEntity producto){
+     public String  registrar(ProductoEntity producto){   
+        
+         String cadena = "";
+        String oldName = producto.getName();
+        String [] split = oldName.split(" ");
+        String newName = "";
+
+        for (int j = 0; j < split.length; j++) {
+            if(j==0)
+                split[j] = split[j].substring(0, 1).toUpperCase() + split[j].substring(1).toLowerCase();
+            else
+                split[j] = split[j].substring(0, 1).toLowerCase()+ split[j].substring(1).toLowerCase();
+            newName = newName + split[j] + " ";
+        }
+        
+           producto.setName(newName);        
+        this.productoRepository.save(producto).toString();
+            
+            cadena = "id = " + producto.getId() + "\nName = " + producto.getName()+ "\nDescripcion = " + producto.getDescription() +
+                     "\nStock = " + producto.getStock() + "\nPrecio Unitario = " + producto.getUnit_price();
+        
+        return cadena;
+          //  return this.productoRepository.save(producto);
+        /*
+        return this.productoRepository.save(producto);
+        */
  
     }
 
